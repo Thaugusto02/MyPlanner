4205bba5098e0c7f93d407b895538a848453bd16
Your Name <thiagoaugusto.20@outlook.com>
2024-08-21 09:23:39 -0300

    adhoc: project routes

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,19 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/activities', [ActivityController::class, 'index'])->name('activity.index');
-    Route::post('/activities', [ActivityController::class, 'create'])->name('activity.create');
-    Route::get('/activities/{activity}/edit', [ActivityController::class, 'edit'])->name('activity.edit');
-    Route::patch('/activities/{activity}', [ActivityController::class, 'update'])->name('activity.update');
-    Route::delete('/activities/{activity}', [ActivityController::class, 'destroy'])->name('activity.destroy');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::prefix('/activities')->group(function () {
+        Route::get('/', [ActivityController::class, 'index'])->name('activity.index');
+        Route::post('/', [ActivityController::class, 'create'])->name('activity.create');
+        Route::get('/{activity}/edit', [ActivityController::class, 'edit'])->name('activity.edit');
+        Route::patch('/{activity}', [ActivityController::class, 'update'])->name('activity.update');
+        Route::delete('/{activity}', [ActivityController::class, 'destroy'])->name('activity.destroy');
+    });
+    Route::prefix('/profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
